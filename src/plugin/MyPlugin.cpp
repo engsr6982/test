b3c4b35a6c58a3d1f0d797611029d11af7695d58
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-06-26 14:03:22 +0800

    test

--- src/plugin/MyPlugin.cpp
+++ src/plugin/MyPlugin.cpp
@@ -4,16 +4,25 @@
 #include <memory>
 #include <string>
 
+#include "ll/api/Expected.h"
 #include "ll/api/memory/Hook.h"
 #include "ll/api/plugin/NativePlugin.h"
 #include "ll/api/plugin/RegisterHelper.h"
 
 
+#include "ll/api/command/Command.h"
+#include "ll/api/command/CommandHandle.h"
+#include "ll/api/command/CommandRegistrar.h"
+#include "ll/api/command/Overload.h"
+#include "ll/api/command/OverloadData.h"
 #include "ll/api/service/Bedrock.h"
 #include "ll/api/service/Service.h"
+#include "ll/api/utils/StringUtils.h"
 #include "mc/common/wrapper/SharedPtr.h"
 #include "mc/deps/core/string/HashedString.h"
+#include "mc/enums/d_b_helpers/Category.h"
 #include "mc/nbt/CompoundTag.h"
+#include "mc/server/commands/CommandOrigin.h"
 #include "mc/world/item/Item.h"
 #include "mc/world/item/ItemStackBase.h"
 #include "mc/world/item/registry/ItemRegistry.h"
@@ -29,10 +38,11 @@
 #include <mc/world/level/storage/DBStorage.h>
 
 
-namespace my_plugin {
-
 template <typename T>
 inline T* Global = nullptr;
+
+namespace my_plugin {
+
 
 LL_AUTO_TYPE_INSTANCE_HOOK(
     DBStorageInitEvent,
@@ -48,25 +58,51 @@ LL_AUTO_TYPE_INSTANCE_HOOK(
     return res;
 }
 
+
 static std::unique_ptr<MyPlugin> instance;
-
-MyPlugin& MyPlugin::getInstance() { return *instance; }
-
-bool MyPlugin::load() {
+MyPlugin&                        MyPlugin::getInstance() { return *instance; }
+bool                             MyPlugin::load() {
     getSelf().getLogger().info("Loading...");
-    // Code for loading the plugin goes here.
     return true;
 }
 
+using string = std::string;
+struct PString {
+    string dim;
+};
+
 bool MyPlugin::enable() {
     getSelf().getLogger().info("Enabling...");
-    // Code for enabling the plugin goes here.
 
-    auto& logger = getSelf().getLogger();
-    auto& db     = *Global<DBStorage>;
-    db.forEachKeyWithPrefix("_", ::DBHelpers::Category::All, [&logger](std::string_view a1, std::string_view a2) {
-        logger.info("{}: {}", a1, a2);
+    auto& cmd = ll::command::CommandRegistrar::getInstance().getOrCreateCommand("db");
+
+    cmd.overload().text("li").execute([](CommandOrigin const&, CommandOutput&) {
+        (*Global<DBStorage>)
+            .forEachKeyWithPrefix("", DBHelpers::Category::All, [](std::string_view a1, std::string_view) {
+                std::cout << a1 << std::endl;
+            });
     });
+    cmd.overload<PString>().text("del").required("dim").execute(
+        [](CommandOrigin const&, CommandOutput&, PString const& a1) {
+            (*Global<DBStorage>).deleteData(a1.dim.c_str(), DBHelpers::Category::All);
+        }
+    );
+    cmd.overload<PString>().text("has").required("dim").execute(
+        [](CommandOrigin const&, CommandOutput&, PString const& a1) {
+            auto res = (*Global<DBStorage>).hasKey(a1.dim.c_str(), DBHelpers::Category::All);
+            std::cout << (res ? "true" : "false") << std::endl;
+        }
+    );
+    cmd.overload().text("tr").execute([](CommandOrigin const&, CommandOutput&) {
+        auto& res = (*Global<DBStorage>);
+
+        string p;
+
+        res.loadData("plot", p, DBHelpers::Category::All);
+
+        res.saveData("Overworld", string(p), DBHelpers::Category::All);
+    });
+
 
     return true;
 }
